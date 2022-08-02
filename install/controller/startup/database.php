9a46ba543778cf4b53177d9ca91d34b68b16c352
Vidushi Rastogi <107478768+vidushi-rastogi99@users.noreply.github.com>
2022-08-02 13:53:56 +0530

    Update database.php

--- install/controller/startup/database.php
+++ install/controller/startup/database.php
@@ -5,18 +5,21 @@ class Database extends \Opencart\System\Engine\Controller {
 		echo DIR_OPENCART;
 		if (is_file(DIR_OPENCART . 'config.php')) {
 			$config = [];
-
+			echo DIR_OPENCART;
 			$lines = file(DIR_OPENCART . 'config.php');
-
+			echo $lines;
 			foreach ($lines as $number => $line) {
+				echo $line;
 				if (strpos(strtoupper($line), 'DB_') !== false && preg_match('/define\(\'(.*)\',\s+\'(.*)\'\)/', $line, $match, PREG_OFFSET_CAPTURE)) {
 					define($match[1][0], $match[2][0]);
 				}
 			}
 
 			if (defined('DB_PORT')) {
+				echo DB_PORT;
 				$port = DB_PORT;
 			} else {
+				echo 'No port';
 				$port = ini_get('mysqli.default_port');
 			}
 			
